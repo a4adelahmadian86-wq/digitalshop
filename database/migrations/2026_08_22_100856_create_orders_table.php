39ebe313fa7ea7385b293ae180100839a8319d74
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-01 18:20:54 +0330

    Upload database/migrations/2026_08_22_100856_create_orders_table.php

---
+++ database/migrations/2026_08_22_100856_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('order_number', 30)->unique();
+            $table->string('status', 20)->default('pending');
+            $table->decimal('subtotal', 12, 0)->default(0);
+            $table->decimal('discount', 12, 0)->default(0);
+            $table->decimal('total', 12, 0)->default(0);
+            $table->timestamp('paid_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
