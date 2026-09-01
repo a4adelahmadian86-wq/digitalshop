8191c1224431c0823f342ecaea9fc93231528292
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-01 18:17:10 +0330

    Upload app/Models/Admin.php

---
+++ app/Models/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'username',
+        'password',
+        'is_active',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+}
