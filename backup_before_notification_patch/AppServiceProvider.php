ea698950e0ab5caf584f207c303c8f2d5207c7e6
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-01 18:18:27 +0330

    Upload backup_before_notification_patch/AppServiceProvider.php

---
+++ backup_before_notification_patch/AppServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Services\Storage\StorageManager;
+use App\Services\Sms\KPanelSmsProvider;
+use App\Services\Sms\SmsProviderInterface;
+use App\Services\Sms\TestSmsProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->singleton(
+            StorageManager::class,
+            function () {
+                return new StorageManager();
+            }
+        );
+
+        $this->app->bind(
+            SmsProviderInterface::class,
+            function () {
+
+                return match (
+                    config('services.sms_provider')
+                ) {
+
+                    'kpanel' =>
+                        new KPanelSmsProvider(),
+
+                    default =>
+                        new TestSmsProvider(),
+
+                };
+            }
+        );
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
