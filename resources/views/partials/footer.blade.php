20e251aa9e6f9286403ad751c1edcada01231300
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-01 18:24:43 +0330

    Upload resources/views/partials/footer.blade.php

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+<div class="container footer-grid">
+
+<div>
+<h3>درباره فایل‌مارکت</h3>
+<p class="muted">
+فروشگاه فایل‌های دیجیتال و محصولات الکترونیکی کاربردی.
+</p>
+</div>
+
+<div>
+<h3>دسترسی سریع</h3>
+<ul>
+<li><a href="{{ route('home') }}">خانه</a></li>
+<li><a href="{{ route('products.index') }}">فروشگاه</a></li>
+<li><a href="#">وبلاگ</a></li>
+<li><a href="#">تماس با ما</a></li>
+</ul>
+</div>
+
+<div>
+<h3>راهنما</h3>
+<ul>
+<li><a href="#">قوانین خرید</a></li>
+<li><a href="#">سوالات متداول</a></li>
+<li><a href="#">پشتیبانی</a></li>
+</ul>
+</div>
+
+</div>
+
+<div class="container copyright">
+© {{ date('Y') }} فایل‌مارکت - تمامی حقوق محفوظ است.
+</div>
+
+</footer>
