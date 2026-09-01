56b3312e477c8e673148f894803cfbd882ca2cb4
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-01 18:20:58 +0330

    Upload database/migrations/2026_08_22_100911_create_payments_table.php

---
+++ database/migrations/2026_08_22_100911_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+            $table->string('gateway', 30);
+            $table->string('transaction_id')->nullable()->index();
+            $table->decimal('amount', 12, 0);
+            $table->string('status', 20)->default('pending');
+            $table->timestamp('paid_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
