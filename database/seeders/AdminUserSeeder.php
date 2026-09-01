163af3e9b5a4038a4872f31e0336d5d74997023f
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-01 18:21:39 +0330

    Upload database/seeders/AdminUserSeeder.php

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            [
+                'phone' => '09151234567',
+            ],
+            [
+                'name' => 'مدیر اصلی',
+                'first_name' => 'مدیر',
+                'last_name' => 'اصلی',
+                'password' => Hash::make('12345678'),
+                'role' => 'admin',
+                'is_active' => true,
+                'phone_verified_at' => now(),
+            ]
+        );
+    }
+}
