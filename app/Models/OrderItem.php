a1906d27f38502780136aabc376cd67c13b528ab
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-01 18:17:22 +0330

    Upload app/Models/OrderItem.php

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'price',
+        'quantity',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function downloads()
+    {
+        return $this->hasMany(
+            DownloadLog::class,
+            'order_item_id'
+        );
+    }
+}
