988f10c66ca0833888bfb2ce72b22d1213b99d0a
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-04 22:10:26 +0330

    Extend routes with dashboard management modules

--- routes/reader.php
+++
@@ -1,4 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReaderSubscriptionController;
-Route::middleware('auth')->prefix('account/reader')->group(function(){Route::get('/',[ReaderSubscriptionController::class,'index'])->name('account.reader');Route::post('/subscribe',[ReaderSubscriptionController::class,'purchase'])->name('account.reader.purchase');});
